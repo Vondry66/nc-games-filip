32db5cbe080e8639c63c32e22cc508b7236ff01d
vondry66 <vondry66@gmail.com>
2022-10-28 18:01:15 +0100

    this adds category and updated home

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,24 +1,31 @@
-import { useEffect,useState } from "react";
-import { Link } from "react-router-dom";
-import { getCategory } from "../utils/api";
 
-const Category =()=>{
-    const[categories,setCategories]=useState([])
-    useEffect(()=>{
-        getCategory().then((category)=>{
-            setCategories(category)
-        }).catch(err=>{})
-    },[])
-    return(<main>
-         <h2 className="h2">Read reviews by category</h2><ul>
-        {categories.map((cat)=>{
-            
-            
-           return<li><Link to={`/reviews/${cat.slug}`}>{cat.slug}</Link></li>
 
-        })}
-        </ul>
-    </main>)
-    
+import {useEffect, useState} from "react"
+import {getCategories} from '../utils/api'
+import {Link} from 'react-router-dom'
+
+function Categories () {
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        getCategories()
+        .then(({categories}) => {
+            setCategories(categories)
+            
+        })
+    }, [categories])
+    return (
+        <section>
+               <h3 className="navBarTitle">What would you like to see today?</h3>
+            <nav className="topNavBar">
+            {categories.map(category => {
+                return <li key={category.slug}>
+                    <Link className="navText" to={`/reviews/${category.slug}`}>{category.slug}</Link>
+                                    
+                </li>
+            })}
+            </nav>
+        </section>
+    )
 }
-export default Category
+export default Categories
